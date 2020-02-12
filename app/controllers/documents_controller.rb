8eaad2fb5bde2e28498cb84ae02631c73eee92d0
marcs16 <luis.edo1234@gmail.com>
2020-02-12 12:14:22 -0500

    Controlador documentos y rutas

---
+++ app/controllers/documents_controller.rb
@@ -0,0 +1,29 @@
+class DocumentsController < ApplicationController
+
+	def index
+	end
+	def new	
+	end
+
+	def show
+	end
+
+	def create
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+
+	def find_document
+		
+	end
+
+	def document_params
+	end
+
+end
