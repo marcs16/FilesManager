66fb6d3407833cddfeb5f1c190c425480b87a644
marcs16 <luis.edo1234@gmail.com>
2020-02-13 17:02:17 -0500

    Vistas y acciones documentos

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,14 +1,23 @@
 class DocumentsController < ApplicationController
-
+	before_action :find_document, only: [:show,:edit,:update,:destroy]
 	def index
+		@documents = Document.all.order("created_at DESC")
 	end
 	def new	
+		@document = Document.new
 	end
 
 	def show
+
 	end
 
 	def create
+		@document = Document.new(document_params)
+		if @document.save
+			redirect_to  @document
+		else
+			render 'new'
+		end
 	end
 
 	def update
@@ -20,10 +29,11 @@ class DocumentsController < ApplicationController
 	private
 
 	def find_document
-		
+		@document = Document.find(params[:id])
 	end
 
 	def document_params
+		params.require(:document).permit(:title,:content)
 	end
 
 end
