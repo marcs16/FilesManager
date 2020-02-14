07f7a8ef0718119137b4409ec9b2d12423be1b73
marcs16 <luis.edo1234@gmail.com>
2020-02-14 12:09:27 -0500

    usuarios solo ven sus documentos

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,7 +1,7 @@
 class DocumentsController < ApplicationController
 	before_action :find_document, only: [:show,:edit,:update,:destroy]
 	def index
-		@documents = Document.all.order("created_at DESC")
+		@documents = Document.where(user_id: current_user).all.order("created_at DESC")
 	end
 	def new	
 		@document = current_user.documents.build
