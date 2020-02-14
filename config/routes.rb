8d178908c4d06536eec45f5156c27186bfcd88c5
marcs16 <luis.edo1234@gmail.com>
2020-02-14 12:25:52 -0500

    Cambio de rutas para usuarios logueados

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
-  root 'welcome#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :documents
+  authenticated :user do
+  	root "documents#index", as: "authenticated_root"
+  end
+  root 'welcome#index'
 end
